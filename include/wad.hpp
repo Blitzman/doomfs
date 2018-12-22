0024ad15369eba72a051beb1829c0c6ef304e70e
Albert Garcia-Garcia <agarcia@dtic.ua.es>
2018-12-23 00:16:15 +0100

    add directory entry reading routine to WAD class
    A new routine read_directory has been added. It reads the whole WAD
    directory entry by entry and interprets each three fields for each
    one of them (name, offset, and size) and stores each entry in an
    std::vector holding the directory for later use.
    
    Also added a pre-allocation instruction for the directory once the
    WAD header has been read so we know the number of lumps (directory
    entries) a priori.

--- include/wad.hpp
+++ include/wad.hpp
@@ -6,18 +6,31 @@
 #include <iostream>
 #include <memory>
 #include <ostream>
+#include <vector>
 
 #include "readers.hpp"
 
-#define WAD_TYPE_LENGTH 4
-#define WAD_LUMP_COUNT_LENGTH 4
-#define WAD_DIRECTORY_OFFSET_LENGTH 4
+#define WAD_HEADER_TYPE_LENGTH 4
+#define WAD_HEADER_LUMPCOUNT_LENGTH 4
+#define WAD_HEADER_OFFSET_LENGTH 4
+#define WAD_ENTRY_OFFSET_LENGTH 4
+#define WAD_ENTRY_SIZE_LENGTH 4
+#define WAD_ENTRY_NAME_LENGTH 8
+
+// http://www.gamers.org/dhs/helpdocs/dmsp1666.html
 
 struct WADHeader
 {
 	std::string type;
 	unsigned int lump_count;
 	unsigned int directory_offset;
+};
+
+struct WADEntry
+{
+	unsigned int offset;
+	unsigned int size;
+	std::string name;
 };
 
 
@@ -28,8 +41,15 @@ class WAD
 		WAD(const std::string & filename)
 		{
 			m_offset = 0;
+
+			// Load the whole WAD file into memory, it only takes a few MiBs
 			load_wad(filename);
+
 			read_header();
+
+			// Pre-allocate directory since we know its size from the header
+			m_directory.reserve(m_wad_header.lump_count);
+			read_directory();
 		}
 
 		friend std::ostream& operator<<(std::ostream& rOs, const WAD& rWad)
@@ -38,6 +58,11 @@ class WAD
 			rOs << "Type: " << rWad.m_wad_header.type << "\n";
 			rOs << "Lump Count: " << rWad.m_wad_header.lump_count << "\n";
 			rOs << "Directory Offset: " << rWad.m_wad_header.directory_offset << "\n";
+
+			rOs << "Directory:\n";
+
+			for (unsigned int i = 0; i < rWad.m_wad_header.lump_count; ++i)
+				rOs << rWad.m_directory[i].name << " " << rWad.m_directory[i].size << "\n";
 		}
 
 	private:
@@ -68,18 +93,50 @@ class WAD
 		{
 			assert(m_wad_data);
 
-			copy_and_capitalize_buffer(m_wad_header.type, m_wad_data, m_offset, WAD_TYPE_LENGTH);
-			m_offset += WAD_TYPE_LENGTH;
+			// The header is a 12-byte component which consists of three 4-byte parts:
+			//	(a) an ASCII string (4-byte) which is "IWAD" or "PWAD"
+			//	(b)	an unsigned int (4-byte) to hold the number of lumps in the WAD file
+			//	(c) an unsigned int (4-byte) that indicates the file offset to the start of the directory
+
+			copy_and_capitalize_buffer(m_wad_header.type, m_wad_data, m_offset, WAD_HEADER_TYPE_LENGTH);
+			m_offset += WAD_HEADER_TYPE_LENGTH;
 
 			m_wad_header.lump_count = read_uint(m_wad_data, m_offset);
-			m_offset += WAD_LUMP_COUNT_LENGTH;
+			m_offset += WAD_HEADER_LUMPCOUNT_LENGTH;
 
 			m_wad_header.directory_offset = read_uint(m_wad_data, m_offset);
+			m_offset += WAD_HEADER_OFFSET_LENGTH;
+		}
+
+		void read_directory()
+		{
+			assert(m_wad_data);
+			assert(m_wad_header.lump_count != 0);
+
+			// The directory has one 16-byte entry for every lump. Each entry consists of three parts:
+			//	(a) an unsigned int (4-byte) which indicates the file offset to the start of the lump
+			//	(b) an unsigned int (4-byte) which indicates the size of the lump in bytes
+			//	(c) an ASCII string (8-byte) which holds the name of the lump (padded with zeroes)
+
+			m_offset = m_wad_header.directory_offset;
+			for (unsigned int i = 0; i < m_wad_header.lump_count; ++i)
+			{
+				WADEntry entry_;
+				entry_.offset = read_uint(m_wad_data, m_offset);
+				m_offset += WAD_ENTRY_OFFSET_LENGTH;
+				entry_.size = read_uint(m_wad_data, m_offset);
+				m_offset += WAD_ENTRY_SIZE_LENGTH;
+				copy_and_capitalize_buffer(entry_.name, m_wad_data, m_offset, 8);
+				m_offset += WAD_ENTRY_NAME_LENGTH;
+
+				m_directory.push_back(entry_);
+			}
 		}
 
 		WADHeader m_wad_header;
 		unsigned int m_offset;
 		std::unique_ptr<uint8_t[]> m_wad_data;
+		std::vector<WADEntry> m_directory;
 };
 
 #endif
